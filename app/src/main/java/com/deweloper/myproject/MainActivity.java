8789f2b64825ab7e31a540f764ef5f246b0f1620
shobirin <ahmadshobirin.dev@gmail.com>
2019-12-25 13:56:36 +0700

    add onclick listener

--- app/src/main/java/com/deweloper/myproject/MainActivity.java
+++ app/src/main/java/com/deweloper/myproject/MainActivity.java
@@ -3,12 +3,40 @@ package com.deweloper.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username,password;
+    Button btnshowtoast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btnshowtoast = findViewById(R.id.show_to_toast);
+
+        btnshowtoast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                String s = user+" "+pass;
+
+
+                Toast.makeText(
+                    getApplicationContext(),
+                    s,
+                    Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 }
